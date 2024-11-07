be2ea6f69266431e1437de2619af86b5ffbc7607
https://github.com/Mahalakshmi-Iyer/British-Airways-Dashboard.git <mahalakshmi.a.iyer@gmail.com>
2024-11-07 13:08:47 +0530

    Day3 : Variables and Data Types

---
+++ VariablesAndDataTypes/AverageOf3.java
@@ -0,0 +1,18 @@
+package Day3;
+
+import java.util.*;
+
+public class AverageOf3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter 3 numbers : ");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        int sum = (a + b + c);
+        int avg = sum / 3;
+
+        System.out.println("The average of " + a + "," + b + " and " + c + " is " + avg);
+    }
+}
---
+++ VariablesAndDataTypes/TotalPrice.java
@@ -0,0 +1,22 @@
+package Day3;
+
+import java.util.*;
+
+public class TotalPrice {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the price of pencil : ");
+        float p1 = sc.nextFloat();
+        System.out.println("Enter the price of pen : ");
+        float p2 = sc.nextFloat();
+        System.out.println("Enter the price of eraser : ");
+        float p3 = sc.nextFloat();
+
+        float total = p1 + p2 + p3;
+        float gst = (18 * total) / 100;
+
+        System.out.println("Total Price : " + total);
+        System.out.println("Net amount : " + (total + gst));
+
+    }
+}
