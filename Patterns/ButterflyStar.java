9736109cfd2c0f1a70c0ddfb6b10bc9ebcd4586c
https://github.com/Mahalakshmi-Iyer/British-Airways-Dashboard.git <mahalakshmi.a.iyer@gmail.com>
2024-11-08 23:28:59 +0530

    Day11 : Patterns

---
+++ Patterns/ButterflyStar.java
@@ -0,0 +1,34 @@
+package Patterns;
+
+public class ButterflyStar {
+    public static void main(String[] args) {
+        int n = 3;
+        int stars;
+        int spaces = 2 * n - 3;
+        for (int i = 1; i <= (2 * n - 1); i++) {
+            stars = i;
+            if (i > n) {
+                stars = 2 * n - i;
+            }
+            for (int j = 1; j <= stars; j++) {
+                System.out.print("* ");
+            }
+            for (int k = 1; k <= spaces; k++) {
+                System.out.print("  ");
+            }
+            for (int j = 1; j <= stars; j++) {
+                if (i == n && j == stars) {
+                    continue;
+                }
+                System.out.print("* ");
+            }
+            System.out.println();
+            if (i < n) {
+                spaces -= 2;
+            } else {
+                spaces += 2;
+            }
+
+        }
+    }
+}
---
+++ Patterns/PalindromicNumberPyramid.java
@@ -0,0 +1,20 @@
+package Patterns;
+
+public class PalindromicNumberPyramid {
+    public static void main(String[] args) {
+        int n = 5;
+
+        for (int i = 1; i <= n; i++) {
+            for (int k = 1; k <= (n - i); k++) {
+                System.out.print("  ");
+            }
+            for (int j = i; j >= 1; j--) {
+                System.out.print(j + " ");
+            }
+            for (int l = 2; l <= i; l++) {
+                System.out.print(l + " ");
+            }
+            System.out.println();
+        }
+    }
+}
